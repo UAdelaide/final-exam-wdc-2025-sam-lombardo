c24fd056bce5a1a0b1058ae97ec1d4a3d323ef1f
Sam Lombardo <sam.lombardo1111@gmail.com>
2025-06-20 03:47:25 +0000

    [automatic commit] 2025-06-20 03:47:25 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; no servers could be reached

--- part1/app.js
+++ part1/app.js
@@ -106,12 +106,11 @@ app.get('/api/walkers/summary', async (req, res) => {
   try {
     const [rows] = await db.execute(`
       SELECT u.username AS walker_username,
-             COUNT(r.rating_id) AS total_ratings,
-             ROUND(AVG(r.rating), 1) AS average_rating,
-             (
-               SELECT COUNT(*) FROM WalkRequests wr
-               JOIN WalkApplications wa ON wa.request_id = wr.request_id
-               WHERE wa.walker_id = u.user_id AND wr.status = 'completed' AND wa.status = 'accepted'
+      COUNT(r.rating_id) AS total_ratings,
+      ROUND(AVG(r.rating), 1) AS average_rating,
+      (SELECT COUNT(*) FROM WalkRequests wr
+      JOIN WalkApplications wa ON wa.request_id = wr.request_id
+      WHERE wa.walker_id = u.user_id AND wr.status = 'completed' AND wa.status = 'accepted'
              ) AS completed_walks
       FROM Users u
       LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
