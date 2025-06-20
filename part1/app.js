316de694fd00044a2dc9ade82f5ca63c061264fe
Sam Lombardo <sam.lombardo1111@gmail.com>
2025-06-20 03:47:46 +0000

    [automatic commit] 2025-06-20 03:47:46 UTC 101.177.76.240

--- part1/app.js
+++ part1/app.js
@@ -108,10 +108,11 @@ app.get('/api/walkers/summary', async (req, res) => {
       SELECT u.username AS walker_username,
       COUNT(r.rating_id) AS total_ratings,
       ROUND(AVG(r.rating), 1) AS average_rating,
-      (SELECT COUNT(*) FROM WalkRequests wr
-      JOIN WalkApplications wa ON wa.request_id = wr.request_id
-      WHERE wa.walker_id = u.user_id AND wr.status = 'completed' AND wa.status = 'accepted'
-             ) AS completed_walks
+      (
+        SELECT COUNT(*) FROM WalkRequests wr
+        JOIN WalkApplications wa ON wa.request_id = wr.request_id
+        WHERE wa.walker_id = u.user_id AND wr.status = 'completed' AND wa.status = 'accepted'
+      ) AS completed_walks
       FROM Users u
       LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
       WHERE u.role = 'walker'
