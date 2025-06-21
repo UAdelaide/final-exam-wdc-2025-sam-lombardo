aa97bd05e147e57bf12392c6ffc2dffbe68d2e9d
Sam Lombardo <sam.lombardo1111@gmail.com>
2025-06-21 06:35:07 +0000

    [automatic commit] 2025-06-21 06:35:07 UTC 101.177.76.240

--- part2/app.js
+++ part2/app.js
@@ -18,6 +18,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
+const dogRoutes = require('./routes/dogRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
