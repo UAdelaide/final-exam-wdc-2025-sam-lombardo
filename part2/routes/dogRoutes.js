5b55c4671be8bbfd723824ee4053b3ff43c37094
Sam Lombardo <sam.lombardo1111@gmail.com>
2025-06-21 06:32:50 +0000

    [automatic commit] 2025-06-21 06:32:50 UTC 101.177.76.240

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
+
+router.get('/', async(req, res) => {
+  try {
+    
+  }catch(err){
+
+  }
+});
+
+module.exports = router;
