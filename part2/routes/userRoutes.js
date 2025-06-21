653a7ea096f2f67dc64a7b7c055a2ea8431819ca
Sam Lombardo <sam.lombardo1111@gmail.com>
2025-06-21 04:51:35 +0000

    [automatic commit] 2025-06-21 04:51:35 UTC 101.177.76.240

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -51,7 +51,6 @@ router.post('/login', async (req, res) => {
 
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
-    console.error(error);
     res.status(500).json({ error: 'Login failed' });
   }
 });
