64b66c52102875dd447fc4cc455924a9c50cf4a5
Sam Lombardo <sam.lombardo1111@gmail.com>
2025-06-21 06:33:42 +0000

    [automatic commit] 2025-06-21 06:33:42 UTC ;; communications error to 208.67.222.222#53: timed out 101.177.76.240

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -5,7 +5,7 @@ const db = require('../models/db');
 router.get('/', async(req, res) => {
   try {
     const [rows] = await db.execute(`
-    
+      SELECT d.dog_id, d.name AS dog_name, d.size, 
     `)
   }catch(err){
 
